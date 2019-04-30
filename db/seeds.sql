9279b1d4b46d65389d6260c7b84db08a4435e7a0
Chris Paul <cpaul319@users.noreply.github.com>
2019-04-30 15:26:03 -0400

    working app, need to test a lot more

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,5 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO cats (name) VALUES ('Cheeseburger',true);
+INSERT INTO cats (name) VALUES ('BaconBurger',true);
+INSERT INTO cats (name) VALUES ('Veggie',true);
+
+ 
